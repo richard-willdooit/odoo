9eb5db14656ffcba3e413c4ef63b2fc76f4c3ac2
Richard deMeester <richard@willowit.com.au>
2018-12-06 11:39:06 +1100

    Changes for v12 whitelisting

--- odoo/addons/base/models/ir_mail_server.py
+++ odoo/addons/base/models/ir_mail_server.py
@@ -21,6 +21,8 @@ from odoo.exceptions import UserError
 from odoo.tools import ustr, pycompat, formataddr, encapsulate_email, email_domain_extract
 
 
+from odoo.addons.base.models.ir_cron import db_whitelisted
+
 _logger = logging.getLogger(__name__)
 _test_logger = logging.getLogger('odoo.tests')
 
@@ -456,6 +458,10 @@ class IrMailServer(models.Model):
         :return: the Message-ID of the message that was just sent, if successfully sent, otherwise raises
                  MailDeliveryException and logs root cause.
         """
+
+        if not db_whitelisted(self.env.cr.dbname):
+            raise UserError(_("Whitelist Error") + "\n" + _("Database cannot send emails as it is not on the whitelist."))
+
         # Use the default bounce address **only if** no Return-Path was
         # provided by caller.  Caller may be using Variable Envelope Return
         # Path (VERP) to detect no-longer valid email addresses.
