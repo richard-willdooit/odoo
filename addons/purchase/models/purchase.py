9a089a565f2087ca35014173011f0bfd49358904
Richard deMeester <richard@willowit.com.au>
2019-11-21 10:57:16 +1100

    Remove compute not used.

--- addons/purchase/models/purchase.py
+++ addons/purchase/models/purchase.py
@@ -31,16 +31,6 @@ class PurchaseOrder(models.Model):
                 'amount_total': amount_untaxed + amount_tax,
             })
 
-    @api.depends('order_line.date_planned')
-    def _compute_date_planned(self):
-        for order in self:
-            min_date = False
-            for line in order.order_line:
-                if not min_date or line.date_planned < min_date:
-                    min_date = line.date_planned
-            if min_date:
-                order.date_planned = min_date
-
     @api.depends('state', 'order_line.qty_invoiced', 'order_line.qty_received', 'order_line.product_qty')
     def _get_invoiced(self):
         precision = self.env['decimal.precision'].precision_get('Product Unit of Measure')
