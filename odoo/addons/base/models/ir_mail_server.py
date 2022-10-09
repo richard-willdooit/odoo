6d013ae4e60b09457e0f1dbaa3cfbab95669c516
Richard deMeester <richard@willdooit.com>
2022-10-09 17:03:58 +0500

    Whitelisting dbs for email and crons
    fix email whilelist related test failures when installing base

--- odoo/addons/base/models/ir_mail_server.py
+++ odoo/addons/base/models/ir_mail_server.py
@@ -19,11 +19,13 @@ from socket import gaierror, timeout
 from OpenSSL import crypto as SSLCrypto
 from OpenSSL.crypto import Error as SSLCryptoError, FILETYPE_PEM
 from OpenSSL.SSL import Error as SSLError
-from urllib3.contrib.pyopenssl import PyOpenSSLContext
+from unittest.mock import MagicMock
 
 from odoo import api, fields, models, tools, _
 from odoo.exceptions import UserError
 from odoo.tools import ustr, pycompat, formataddr, email_normalize, encapsulate_email, email_domain_extract, email_domain_normalize
+
+from odoo.addons.base.models.ir_cron import db_whitelisted
 
 
 _logger = logging.getLogger(__name__)
@@ -696,6 +698,14 @@ class IrMailServer(models.Model):
         if self._is_test_mode():
             _test_logger.info("skip sending email in test mode")
             return message['Message-Id']
+
+        # Some odoo tests in base explicitly patch ir.mail_server to return False from _is_test_mode()
+        if (
+            not db_whitelisted(self.env.cr.dbname)
+            and not isinstance(self.connect, MagicMock)
+            and smtp.__class__.__name__ != "FakeSMTP"
+        ):
+            raise UserError(_("Whitelist Error") + "\n" + _("Database cannot send emails as it is not on the whitelist."))
 
         try:
             message_id = message['Message-Id']
