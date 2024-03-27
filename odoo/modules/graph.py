24b2fe7170a60d4cd68e92049c75cbde93e7e66c
James Bos <james.bos@willdooit.com>
2024-03-28 10:56:39 +1100

    Ensure unmet depends are logged as warnings

--- odoo/modules/graph.py
+++ odoo/modules/graph.py
@@ -140,7 +140,7 @@ class Graph(dict[str, 'Node']):
 
         for package in later:
             unmet_deps = [p for p in dependencies[package] if p not in self]
-            _logger.info('module %s: Unmet dependencies: %s', package, ', '.join(unmet_deps))
+            _logger.warning('module %s: Unmet dependencies: %s', package, ', '.join(unmet_deps))
 
         return len(self) - len_graph
 
